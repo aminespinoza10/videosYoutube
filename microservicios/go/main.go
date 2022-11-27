b346d95cd1898d467fa7e62d4a2c68f34e3f01ac
aminespinoza10 <amin.espinoza@outlook.com>
2022-11-27 16:32:20 -0600

    go microservice added

---
+++ microservicios/go/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+)
+
+func main() {
+
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/weatherForecast", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Today is gonna be a great sunny day for a go service!")
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+}
